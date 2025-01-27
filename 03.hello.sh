2bc05ad116b9afc19c39e486771e8601e0c6b83d
harsha <harshadoddapaneni1@gmail.com>
2025-01-27 11:42:11 +0530

    variables

---
+++ 03.hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1:hi ${PERSON2},how are u?"
+echo "${PERSON2}:hello $PERSON1, iam fine"
--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+#Author - harsha
+#team -devops
 echo "hello world"
