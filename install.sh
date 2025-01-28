2443634ed252979c25b2f74f77e4341e30e345ee
harsha <harshadoddapaneni1@gmail.com>
2025-01-28 08:56:29 +0530

    loops

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo "userid is: $USERID"
+#echo "userid is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "plese run this script with root priveleges"
+fi
+
+dnf install git -y
