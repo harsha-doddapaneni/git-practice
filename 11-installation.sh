e5f66180b0d99af02e282555c71ac426b16d9f30
harsha <harshadoddapaneni1@gmail.com>
2025-01-29 10:52:58 +0530

    dev

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,30 +5,32 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ] #0
 then
     echo "please run this script with root priveleges"
-    exit 0
+    exit 1
 fi
 
 echo $? #0
 
-#dnf list installed git
+dnf list installed git
 
-# if [ $? -ne 0 ]
-# then
-#     echo "git is not installed, going to install it.."
-#     dnf install git -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "git installation is not success..check it"
-#         exit 1
-#     else
-#         echo "git installation is success"
-#     fi
-# else
-#     echo "git is already installed, nothing to install it.."
-# fi
+if [ $? -ne 0 ]
+then
+    echo "git is not installed, going to install it.."
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "git installation is not success..check it"
+        exit 1
+    else
+        echo "git installation is success"
+    fi
+else
+    echo "git is already installed, nothing to install it.."
+fi
 
-# dnf list installed mysql
+echo $? 
 
-# if [ $? -ne 0 ]
-# then
-#     echo "
+#dnf list installed mysql
+
+#if [ $? -ne 0 ]
+#then
+#    echo "
