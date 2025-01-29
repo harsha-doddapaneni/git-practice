04245ee313466008387cf4c776940e972277ec0a
harsha <harshadoddapaneni1@gmail.com>
2025-01-29 10:12:54 +0530

    dev

--- 11-installation.sh
+++ 11-installation.sh
@@ -2,10 +2,12 @@
 
 USERID=$(id -u)
 #echo "USERID is: $USERID"
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] #1001
 then
     echo "please run this script with root priveleges"
     exit 1
 fi
 
-dnf install git -y
+#dnf list installed git
+
+#if [ $? -ne 0 ]
