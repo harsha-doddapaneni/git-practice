3992465f0ed8637ac939ea06c1b8dfa4ac338d75
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-04 16:52:39 +0530

    redirectors

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -10,6 +10,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+M="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
@@ -28,3 +29,16 @@ VALIDATE(){
 }
 
 CHECK_ROOT
+
+for package in $@
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it..." &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "Installing $package"
+    else
+        echo "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
+    fi
+done
