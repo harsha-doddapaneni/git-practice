390399291329bcf3fb0120627d53bd95fd345251
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-04 16:57:35 +0530

    redirectors

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -28,7 +28,17 @@ VALIDATE(){
     fi
 }
 
+USAGE(){
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
+    exit 1 
+}
+
 CHECK_ROOT
+
+if [ $# -eq 0 ]
+then
+    USAGE
+fi
 
 for package in $@
 do
@@ -39,6 +49,6 @@ do
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "Installing $package"
     else
-        echo "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
+        echo -e "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
     fi
 done
