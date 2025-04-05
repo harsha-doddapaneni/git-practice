b34305409dab09f5a13f7291a31296f8bf43e98e
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-05 09:32:52 +0530

    set

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -xe
+set -e
+
+failure(){
+    echo "Failed at: $1:$2"
+}
+
+trap 'failure "${LIINENO}" "$BASH_COMMAND"' ERR
 echo "helo success"
 echoo "helo fail"
 echo "helo after failure"
