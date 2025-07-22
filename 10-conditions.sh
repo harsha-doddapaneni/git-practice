5a8f6ac770004eec911839dae86686adcbe39542
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-22 08:30:34 +0530

    git-practice

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,19 +1,19 @@
 #!bin/bash
 
-NUMBER=$1
+NUMBER1=$1
 
-if [ $NUMBER -gt 20 ]
+if [ $NUMBER1 -gt 20 ]
 then
-    echo "given no: $NUMBER is greater then 20"
+    echo "given no: $NUMBER1 is greater then 20"
 else
-    echo "given no: $NUMBER is less then 20"
+    echo "given no: $NUMBER1 is less then 20"
 fi
 
-NUMBER=$2
+NUMBER2=$2
 
-if [ $NUMBER -lt 30 ]
+if [ $NUMBER2 -lt 30 ]
 then
-    echo "given no: $NUMBER is less then 30"
+    echo "given no: $NUMBER2 is less then 30"
 else
-    echo "given no: $NUMBER is greater than 30"
+    echo "given no: $NUMBER2 is greater than 30"
 fi
