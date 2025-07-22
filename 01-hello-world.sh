0fc7f811eca3ed3a7932f40e8f87b5249d6724e6
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-22 08:01:37 +0530

    git-practice

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Ctrl+S --> save, this is only for info purpose. not part of code
+# Ctrl+S --> save, this is only for info purpose.
 # Author - Sivakumar Reddy
 # Team - DevOps
 echo "Hello world"
