61d6cd966dcf63b9378da64d16cdf64af139e5f3
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-22 08:15:48 +0530

    git-practice

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,6 +4,7 @@ echo "Please enter your username::"
 read  USERNAME #takes input into USERNAME variable
 echo "Username entered is: $USERNAME"
 
+
 echo "Please enter your password::"
 read -s PASSWORD
 echo "Password entered is: $PASSWORD"
