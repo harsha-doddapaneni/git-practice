4d6fc33b8e342d35c9e1ace24b146d0a547591ef
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-10-22 08:05:07 +0530

    git

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,4 +5,4 @@ PERSON2=$2
 echo "$PERSON1:: Hi ${PERSON2}, How are you ?"
 echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing ?"
 echo "$PERSON1:: I am doing good ${PERSON2}. What's going on ?"
-echo "${PERSON2}:: I started learning Shell Script $PERSON1"
+echo "${PERSON2}:: I Started learning Shell Script $PERSON1"
